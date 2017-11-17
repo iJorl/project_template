1ae29df28cbd16ba0d95a47f9ff6bf895658a34a
Joël Mathys <mathys.joel@gmail.com>
2017-11-17 14:48:17 +0100

    change graph and colony generator to array structs

--- code/gen_colony.m
+++ code/gen_colony.m
@@ -1,14 +1,18 @@
-function[cols, ants] = gen_colony()
+function[colonies] = gen_colony()
 
 % place, nrOfAnts, Ants
-cols = [
-    [1, 10]
-];
-ants = [
-]
+colonies = [];
 
-for i=1:1:cols(1,2)
-    ants(i,:) = ant(1, cols(1,1))
-    %x{1}(3)(i)=ant(1,x{1}(1));
-    %x(1){3}(i)=ant(1,x(1));
+colonies(1).pos = 1;
+colonies(1).nrAnts = 10;
+colonies(1).ants = [];
+
+nrColonies = size(colonies,2)
+
+for col=1:1:nrColonies
+
+    for i=1:1:colonies(col).nrAnts
+       colonies(col).ants(i) = gen_ant(col, colonies(col).pos); 
+    end
+    
 end
