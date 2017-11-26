ea78eca369cf4402d217a78a336083940b54b9ea
Marc Rauch <marcrauch@Marcs-MacBook-Pro.local>
2017-11-26 21:57:32 +0100

    initial move script, Uturns still have to be implemented

--- code/ant_move.m
+++ code/ant_move.m
@@ -1,63 +1,122 @@
-function[newAnt] = ant_move(ant, graph, sources, nodes, edges)
+function[newAnt,newSources,newEdges] = ant_move(ant, sources, nodes, edges)
+
+uturnProb = 1000; % Probability of a u-turn if no pheromones on edge
+
+%First time the ant is moved
+if ant.edge == 0
+    %select edge
+    ant.edge = ant_decision(ant, nodes, edges);
+    ant.edgeProgress = edges(ant.edge).weight;
+    % determine if direction is - or +
+    if edges(ant.edge).from == ant.pos
+        ant.direction = 1;
+    else
+        ant.direction = -1;
+    end
+end
 
 % explore graph
-if ant.state == 'explore'
-
+if strcmp(ant.state,'explore')
     % on a node
     if ant.edgeProgress == 0
+        % add new node to the path
+        if ant.direction == 1
+            ant.path(length(ant.path)+1) = edges(ant.edge).to;
+        else
+            ant.path(length(ant.path)+1) = edges(ant.edge).from;
+        end
+        
+        ant.pos = ant.path(length(ant.path));
         % check if discovered food source! - if yes has to switch to back
-        if 
-        
-        
-        %select new edge
-        nextEdge = ant_decision(ant, graph, edges);
-        ant.edge = nextEdge;
-        
-        % determine if direction is - or +
-        if edges(nextEdge).from == ant.pos
-            ant.direction = 1;
-        else
-            ant.direction = -1;
-        end
-    
-        %could be that chosen edge is only length 1
-    
-        if edges(newEdge).weight == 1:
-            ant.edgeProgress = 0;
-        else
-            ant.edgeProgress = 1; 
+        if strcmp(nodes(ant.path(length(ant.path))).type,'source')
+            ant.state = 'back';
+            ant.direction = ant.direction*(-1);
+            ant.edgeProgress = edges(ant.edge).weight;
+            % Adjust quality of the carried food & invrement counter of
+            % source
+            pos = ant.path(length(ant.path));
+            for i=1:length(sources)
+                if sources(i).pos == pos
+                    ant.food = sources(i).quality;
+                    sources(i).antNr = sources(i).antNr+1;
+                end
+            end
+            ant.path = ant.path(1:end-1);
         end
         
+        if strcmp(nodes(ant.path(length(ant.path))).type,'traffic')
+            %select new edge
+            nextEdge = ant_decision(ant, nodes, edges);
+            ant.edge = nextEdge;
+            ant.edgeProgress = edges(nextEdge).weight;
+            % determine if direction is - or +
+            if edges(nextEdge).from == ant.pos
+                ant.direction = 1;
+            else
+                ant.direction = -1;
+            end
+        end
+    
+        % Walk forward on the new edge;
+        ant.edgeProgress = ant.edgeProgress -1;
+           
     else
     % travelling on a edge
         % make a uturn?
-        diffDir = 1;
-        
-        ant.edgeProgress = ant.edgeProgress + diffDir;
-        
-        %at end of edge?
-        if ant.edgeProgress == edges(ant.edge).weight
-           ant.edgeProgress = 0; 
-           %update new position
-           if ant.direction == 1
-              ant.position = edges(ant.edge).to; 
-           else
-               ant.position = edges(ant.edge).from;
-           end
+        if (edges(ant.edge).phermons/edges(ant.edge).weight)+uturnProb<rand() % baaaaad!! need to normalize!!
+            if edges(ant.edge).weight - ant.edgeProgress>0
+            ant.direction = ant.direction *(-1);
+                ant.edgeProgress = edges(ant.edge).weight - ant.edgeProgress;
+            end
         end
-        
+        ant.edgeProgress = ant.edgeProgress-1;     
     end
 
 % back to colony nest
 else
-    if ant.edgeProgress == 0
     % on a node
-    
+    if ant.edgeProgress == 0
+        ant.pos = ant.path(length(ant.path));
+        ant.path = ant.path(1:end-1);
+        % back at home -> turn around 
+        if isempty(ant.path)
+            ant.path(1) = ant.pos;
+            ant.state = 'explore';
+            ant.edge = ant_decision(ant, nodes, edges);
+            ant.edgeProgress = edges(ant.edge).weight;
+            ant.food = 0;
+            if edges(ant.edge).from == ant.pos
+                ant.direction = 1;
+            else
+                ant.direction = -1;
+            end
+        % on a traffic node
+        else
+            % find next edge
+            for i=1:length(nodes(ant.pos).edges)
+                e = nodes(ant.pos).edges(i);
+                if edges(e).from == ant.path(length(ant.path)) || edges(e).to == ant.path(length(ant.path))
+                   ant.edge = e;
+                end
+            end
+            ant.edgeProgress = edges(ant.edge).weight;
+            if edges(ant.edge).from == ant.pos
+                ant.direction = 1;
+            else
+                ant.direction = -1;
+            end
+        end
+        ant.edgeProgress = ant.edgeProgress -1;  
+        edges(ant.edge).phermons = edges(ant.edge).phermons + ant.food;
     else
-    % travelling on a edge
-        %make a uturn
-        diffDir = 1;
-        ant.edgeProgress += diffDir;
+
+        % travelling on a edge
+        ant.edgeProgress = ant.edgeProgress-1;
+        % place phermons 
+        edges(ant.edge).phermons = edges(ant.edge).phermons + ant.food;
     end
 end
 
+newAnt = ant;
+newEdges = edges;
+newSources = sources;
