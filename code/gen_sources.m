a5021801d957bd34e06cbeda0346c03c9a345612
Marc Rauch <marcrauch@Marcs-MacBook-Pro.local>
2017-11-26 21:50:52 +0100

    corrected spelling & position in sources

--- code/gen_sources.m
+++ code/gen_sources.m
@@ -3,12 +3,12 @@ function[x] = gen_sources()
 
 % where located, food quality, #ants
 
-x(1).pos = 2;           % Index of the node in graph
-x(1).quality = 10;      % Quality of the food source
-x(1).anrNr = 0;         % Number of ants at the sources
+x(1).pos = 3;           % Index of the node in graph
+x(1).quality = 0.1;      % Quality of the food source
+x(1).antNr = 0;         % Number of ants at the sources
 
-x(2).pos = 3;           % Index of the node in graph
-x(2).quality = 10;      % Quality of the food source
+x(2).pos = 4;           % Index of the node in graph
+x(2).quality = 0.1;      % Quality of the food source
 x(2).antNr = 0;         % Number of ants at the sources
 
 %x = [
